41ecce048eb149a736d8da5b912a18ea9513267a
abelmuro <abelmuro93@gmail.com>
2023-07-19 18:12:01 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.js';
+
+const root = document.getElementById('root')
+
+const clientRoot = ReactDOM.createRoot(root);  
+clientRoot.render(<App/>);        
